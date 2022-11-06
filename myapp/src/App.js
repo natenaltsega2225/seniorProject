b0311f6ac0c4809db54e877506e5cbe376bd2749
Natenal Tsega <nati2gashaw@gmail.com>
2022-11-06 15:34:22 -0600

    initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
+import Home from './components/pages/Home'
+import AddRecipe from './components/pages/AddRecipe'
+import Favorites from './components/pages/Favorites'
+import Articles from './components/pages/Articles'
+import Contact from './components/pages/Contact'
+import Food from './components/pages/subpages/Food'
+import Cook from './components/pages/subpages/Cook'
+import Recipe from './components/pages/subpages/Recipe'
+import Ingredients from './components/pages/subpages/ingredients';
+import Login from './components/pages/subpages/Login';
+import SignUp from './components/pages/subpages/Signup';
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const App = () => {
+  const auth = getAuth();
+  const [user,setUser] = useState('');
+  const [user, setUser] = useState('')
+  const [isAdmin, setIsadmin] = useState(false)
+
+  useEffect(() => {
+     onAuthStateChanged(auth,(user) = {
+      if (user) {
+        const uid = user.uid;
+        
+        setUser(user)
+         NavigationPreloadManager('/home')
+         console.log("user is here", uid)
+         if (uid == '7kbPy4lOBPc5orYApkmMjoviBu82'){
+          setIsadmin(true)
+         }
+      } else {
+         setUser(false)
+         console.log("user is not here")
+      }
+     })
+  })
+  
+  
+  return (
+    <div className='App'>
+      
+    <Routes>  
+          <Route path='/sign-up'  element={<SignUp/>} />
+          <Route path='/Login'  element={<Login/>} />
+          <Route path='/' exact  element={<Home/>} />
+          <Route path='/Home' exact  element={<Home/>} />
+          {isAdmin && <Route path='/Admin' exact  element={<Admin/>} />}
+          <Route path='/AddRecipe' exact  element={<AddRecipe/>} />
+          <Route path='/Favorites' element={<Favorites/>} />
+          <Route path='/Articles' element={<Articles/>} />
+          <Route path='/Contact' element={<Contact/>} />
+          <Route path='/Recipies' element={<Food/>} />
+          <Route path='/Ingredients' element={<Ingredients/>} />
+          <Route path='/Cook' element={<Cook/>} />
+          <Route path='/Recipe/:id' element={<Recipe/>} />
+    </Routes>
+  
+
+
+</div>
+  );
+}
+
+export default App
---
+++ myapp/src/components/pages/subpages/Cook.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cook() {
+  return (
+    <div>Cook</div>
+  )
+}
+
+export default Cook
