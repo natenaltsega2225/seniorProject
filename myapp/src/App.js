355c6de5bacdc8adbf8f2e267c592bec65c4b8a9
Natenal Tsega <nati2gashaw@gmail.com>
2022-11-28 19:36:10 -0600

    App component updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,56 +13,80 @@ import Recipe from './components/pages/subpages/Recipe'
 import Ingredients from './components/pages/subpages/ingredients';
 import Login from './components/pages/subpages/Login';
 import SignUp from './components/pages/subpages/Signup';
+import Admin from './components/pages/Admin';
+
+
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
 
 const App = () => {
   const auth = getAuth();
-  const [user,setUser] = useState('');
+const navigate = useNavigate();
+  // this state is storing user details.
   const [user, setUser] = useState('')
-  const [isAdmin, setIsadmin] = useState(false)
+  const [isAdmin,setIsadmin] = useState(false)
 
-  useEffect(() => {
-     onAuthStateChanged(auth,(user) = {
+useEffect(()=>{
+// this function is listener for checking user current session 
+    // https://firebase.google.com/docs/auth/web/start
+    onAuthStateChanged(auth, (user) => {
       if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
         const uid = user.uid;
-        
+
         setUser(user)
-         NavigationPreloadManager('/home')
-         console.log("user is here", uid)
-         if (uid == '7kbPy4lOBPc5orYApkmMjoviBu82'){
+        navigate('/home')
+        console.log("user is here",uid)
+        if(uid == '7kbPy4lOBPc5orYApkmMjoviBu82'){
           setIsadmin(true)
-         }
+        }
+
       } else {
-         setUser(false)
-         console.log("user is not here")
+        // User is signed out
+        // ...
+        // navigate('/home')
+        setUser(false)
+
+      console.log("user is not here")
+
       }
-     })
-  })
-  
-  
+    });
+},[auth])
+    
+
+
+
   return (
+
     <div className='App'>
       
-    <Routes>  
-          <Route path='/sign-up'  element={<SignUp/>} />
-          <Route path='/Login'  element={<Login/>} />
-          <Route path='/' exact  element={<Home/>} />
-          <Route path='/Home' exact  element={<Home/>} />
-          {isAdmin && <Route path='/Admin' exact  element={<Admin/>} />}
-          <Route path='/AddRecipe' exact  element={<AddRecipe/>} />
-          <Route path='/Favorites' element={<Favorites/>} />
-          <Route path='/Articles' element={<Articles/>} />
-          <Route path='/Contact' element={<Contact/>} />
-          <Route path='/Recipies' element={<Food/>} />
-          <Route path='/Ingredients' element={<Ingredients/>} />
-          <Route path='/Cook' element={<Cook/>} />
-          <Route path='/Recipe/:id' element={<Recipe/>} />
-    </Routes>
-  
+        <Routes>  
+              <Route path='/sign-up'  element={<SignUp/>} />
+              <Route path='/Login'  element={<Login/>} />
+              <Route path='/' exact  element={<Home/>} />
+              <Route path='/Home' exact  element={<Home/>} />
+              {isAdmin && <Route path='/Admin' exact  element={<Admin/>} />}
+              <Route path='/AddRecipe' exact  element={<AddRecipe/>} />
+              <Route path='/Favorites' element={<Favorites/>} />
+              <Route path='/Articles' element={<Articles/>} />
+              <Route path='/Contact' element={<Contact/>} />
+              <Route path='/Recipies' element={<Food/>} />
+              <Route path='/Ingredients' element={<Ingredients/>} />
+              <Route path='/Cook' element={<Cook/>} />
+              <Route path='/Recipe/:id' element={<Recipe/>} />
+        </Routes>
+      
 
 
-</div>
+    </div>
+
+    
+
+
   );
-}
 
-export default App
+
+
+}
+export default App;
